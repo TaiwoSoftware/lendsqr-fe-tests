18271628b5482a3577dbb89a44ab83d876f6cf63
Taiwo AKerele <davcodes24@gmail.com>
2023-12-05 23:24:37 +0100

    Working on routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
-import './App.css'
-import SignUp from './component/SignUp'
-
+import "./App.css";
+import SignUp from "./component/SignUp";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 function App() {
-
   return (
     <>
-      <SignUp />
+      <Router>
+      <Routes>
+        <Route path="/" />
+      </Routes>
+        <SignUp />
+      </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
